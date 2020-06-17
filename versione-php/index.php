24bc4bb0c22393195c190e9582f71dfd78e66c75
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-06-17 16:27:08 +0200

    Creazione file Laravel Mix

---
+++ versione-php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Ajax Dischi</title>
+        <link rel="stylesheet" href="public/app.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
